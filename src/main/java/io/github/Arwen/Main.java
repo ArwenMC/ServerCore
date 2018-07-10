0b02ddf1fafa3d3cb7e5dd01a9032c43d1da235c
Alexander Milanovich <steave0982@outlook.com>
2018-07-09 23:08:43 -0400

    Added GamemodeCommand

--- src/main/java/io/github/Arwen/Main.java
+++ src/main/java/io/github/Arwen/Main.java
@@ -1,6 +1,7 @@
 package io.github.Arwen;
 
 import io.github.Arwen.commands.FlyCommand;
+import io.github.Arwen.commands.GamemodeCommand;
 import io.github.Arwen.events.PlayerDisconect;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -14,9 +15,22 @@ public class Main extends JavaPlugin implements Listener {
     public String notPlayer = ChatColor.RED + "You must be a player to use this command.";
     public String noPermission = ChatColor.RED + "You do not have permission to use this command.";
     public String prefix = getConfig().getString("Messages.Prefix").replace('&', '§');
+    public String notOnline = getConfig().getString("Messages.PlayerOffline").replace('&', '§');
+
 
     public String flyEnabled = getConfig().getString("Messages.FlyEnabled").replace('&', '§');
     public String flyDisabled = getConfig().getString("Messages.FlyDisabled").replace('&', '§');
+
+    public String gm0 = getConfig().getString("Gamemode.GM0").replace('&', '§');
+    public String gm1 = getConfig().getString("Gamemode.GM1").replace('&', '§');
+    public String gm2 = getConfig().getString("Gamemode.GM2").replace('&', '§');
+    public String gm3 = getConfig().getString("Gamemode.GM3").replace('&', '§');
+
+    public String actionbar0 = getConfig().getString("Gamemode.GM0_ActionBar").replace('&', '§');
+    public String actionbar1 = getConfig().getString("Gamemode.GM1_ActionBar").replace('&', '§');
+    public String actionbar2 = getConfig().getString("Gamemode.GM2_ActionBar").replace('&', '§');
+    public String actionbar3 = getConfig().getString("Gamemode.GM3_ActionBar").replace('&', '§');
+
 
     public Permission staffFly = new Permission("core.fly");
     public Permission Admin = new Permission("core.admin");
@@ -37,6 +51,7 @@ public class Main extends JavaPlugin implements Listener {
         saveDefaultConfig();
 
         getCommand("fly").setExecutor(new FlyCommand(this));
+        getCommand("gamemode").setExecutor(new GamemodeCommand(this));
         getServer().getPluginManager().registerEvents(new PlayerDisconect(this), this);
 
     }
---
+++ src/main/java/io/github/Arwen/commands/GamemodeCommand.java
@@ -0,0 +1,110 @@
+package io.github.Arwen.commands;
+
+
+import io.github.Arwen.Main;
+import io.github.Arwen.api.ActionBar;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class GamemodeCommand implements CommandExecutor {
+    Main plugin;
+
+    public GamemodeCommand(Main instance) {
+        plugin = instance;
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String l, String[] args)
+    {
+        Player player = (Player)sender;
+        if (cmd.getName().equalsIgnoreCase("gamemode")) {
+            if (player.hasPermission(plugin.Admin)) {
+                if (args.length == 0)
+                {
+
+                    {
+
+                    }
+                }
+                else if (args.length == 1)
+                {
+                    if ((args[0].equalsIgnoreCase("0")) || (args[0].equalsIgnoreCase("survival")))
+                    {
+                        player.setGameMode(GameMode.SURVIVAL);
+                        player.sendMessage(plugin.prefix + plugin.gm0);
+                        ActionBar.send(player, plugin.actionbar0);
+                    }
+                    if ((args[0].equalsIgnoreCase("1")) || (args[0].equalsIgnoreCase("creative")))
+                    {
+                        player.setGameMode(GameMode.CREATIVE);
+                        player.sendMessage(plugin.prefix + plugin.gm1);
+                        ActionBar.send(player, plugin.actionbar1);
+                    }
+                    if ((args[0].equalsIgnoreCase("2")) || (args[0].equalsIgnoreCase("adventure")))
+                    {
+                        player.setGameMode(GameMode.ADVENTURE);
+                        player.sendMessage(plugin.prefix + plugin.gm2);
+                        ActionBar.send(player, plugin.actionbar2);
+                    }
+                    if ((args[0].equalsIgnoreCase("3")) || (args[0].equalsIgnoreCase("spectator")))
+                    {
+                        player.setGameMode(GameMode.SPECTATOR);
+                        player.sendMessage(plugin.prefix+ plugin.gm3);
+                        ActionBar.send(player, plugin.actionbar3);
+                    }
+                }
+                else if (args.length == 2)
+                {
+                    Player t = Bukkit.getPlayer(args[1]);
+                    if (t == null)
+                    {
+                        player.sendMessage(plugin.prefix + plugin.notOnline);
+                        return true;
+                    }
+                    if ((args[0].equalsIgnoreCase("0")) || (args[0].equalsIgnoreCase("survival")))
+                    {
+                        t.setGameMode(GameMode.SURVIVAL);
+                        t.sendMessage(plugin.prefix + plugin.gm0);
+                        ActionBar.send(t, plugin.actionbar0);
+                    }
+                    if ((args[0].equalsIgnoreCase("1")) || (args[0].equalsIgnoreCase("creative")))
+                    {
+                        t.setGameMode(GameMode.CREATIVE);
+                        t.sendMessage(plugin.prefix + plugin.gm1);
+                        ActionBar.send(t, plugin.actionbar1);
+                    }
+                    if ((args[0].equalsIgnoreCase("2")) || (args[0].equalsIgnoreCase("adventure")))
+                    {
+                        t.setGameMode(GameMode.ADVENTURE);
+                        t.sendMessage(plugin.prefix + plugin.gm2);
+                        ActionBar.send(t, plugin.actionbar2);
+                    }
+                    if ((args[0].equalsIgnoreCase("3")) || (args[0].equalsIgnoreCase("spectator")))
+                    {
+                        t.setGameMode(GameMode.SPECTATOR);
+                        t.sendMessage(plugin.prefix + plugin.gm3);
+                        ActionBar.send(t, plugin.actionbar3);
+                    }
+                }
+                else
+                {
+                    List<String> message = plugin.getConfig().getStringList("Gamemode.Usage");
+                    for (String msg : message)
+                    {
+                        msg =
+                                msg.replace('&', '§').replace("{prefix}", plugin.prefix);
+                        player.sendMessage(plugin.prefix + msg);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
+
