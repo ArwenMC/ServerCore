1b70c3f8c3b31268b387bdb2bcafc4e63104b2b7
Christopher Nethercott <ccnethercott@gmail.com>
2018-07-21 13:55:48 +0100

    Begone old shitty code

--- src/main/java/com/arwenmc/ServerCore.java
+++ src/main/java/com/arwenmc/ServerCore.java
@@ -25,48 +25,6 @@ public class ServerCore extends JavaPlugin implements Listener {
     static MongoClient client;
 
 
-    public String notPlayer = ChatColor.RED + "You must be a player to use this command.";
-    public String noPermission = ChatColor.RED + "You do not have permission to use this command.";
-    public String prefix = getConfig().getString("Messages.Prefix").replace('&', '§');
-
-    public boolean spawnSound = getConfig().getBoolean("Spawn.Explosion_Sound_On_Spawn");
-    public String spawnMessage = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Spawn.Message"));
-    public String setSpawnMessage = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Spawn.Set_Spawn_Message"));
-
-    public String actionbarwelcomeback = getConfig().getString("Messages.Actionbar_Welcome_Back").replace('&', '§');
-    public String actionbarwelcome = getConfig().getString("Messages.Actionbar_Welcome").replace('&', '§');
-    public String welcomeback = getConfig().getString("Messages.Welcome_Back").replace('&', '§');
-    public String welcome = getConfig().getString("Messages.Welcome").replace('&', '§');
-    public String notOnline = getConfig().getString("Messages.PlayerOffline").replace('&', '§');
-
-
-    public String flyEnabled = getConfig().getString("Messages.FlyEnabled").replace('&', '§');
-    public String flyDisabled = getConfig().getString("Messages.FlyDisabled").replace('&', '§');
-
-    public String gm0 = getConfig().getString("Gamemode.GM0").replace('&', '§');
-    public String gm1 = getConfig().getString("Gamemode.GM1").replace('&', '§');
-    public String gm2 = getConfig().getString("Gamemode.GM2").replace('&', '§');
-    public String gm3 = getConfig().getString("Gamemode.GM3").replace('&', '§');
-
-    public String actionbar0 = getConfig().getString("Gamemode.GM0_ActionBar").replace('&', '§');
-    public String actionbar1 = getConfig().getString("Gamemode.GM1_ActionBar").replace('&', '§');
-    public String actionbar2 = getConfig().getString("Gamemode.GM2_ActionBar").replace('&', '§');
-    public String actionbar3 = getConfig().getString("Gamemode.GM3_ActionBar").replace('&', '§');
-
-    public boolean chatMuted;
-
-    public String host = this.getConfig().getString("MySQL.Host");
-    public int port = this.getConfig().getInt("MySQL.Port");
-    public String database = this.getConfig().getString("MySQL.Database");
-    public String username = this.getConfig().getString("MySQL.Username");
-    public String password = this.getConfig().getString("MySQL.Password");
-
-    public Permission staffFly = new Permission("core.fly");
-    public Permission Admin = new Permission("core.admin");
-
-    Permission[] permissions = {
-            staffFly, Admin
-    };
 
 
     @Override
