c2056967984ee4ee6bfa4fc5e921cfe89e79596a
Christopher Nethercott <ccnethercott@gmail.com>
2018-07-21 14:14:29 +0100

    Started getting config although this does seem repetive. might write a method

--- src/main/java/com/arwenmc/ServerCore.java
+++ src/main/java/com/arwenmc/ServerCore.java
@@ -1,13 +1,27 @@
 package com.arwenmc;
 
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ServerCore extends JavaPlugin {
+
+    private FileConfiguration config = this.getConfig();
+
+    public String NOT_PLAYER = colour(config.getString("general.not_player"));
+    public String NO_PERMISSION = colour(config.getString("general.no_permission"));
+    public String MISSING_ARGUMENT = colour(config.getString("general.missing_argument"));
+    public String UNKOWN_ARGUMENT = colour(config.getString("general.unknown_argument"));
+    public String PREFIX = colour(config.getSTr)
 
     @Override
     public void onEnable() {}
 
     @Override
     public void onDisable() {}
+
+    private String colour(String input) {
+        return ChatColor.translateAlternateColorCodes('&', input);
+    }
 
 }
