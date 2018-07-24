4e5e102378d204942c88581b15c039e1089f9794
Christopher Nethercott <ccnethercott@gmail.com>
2018-07-24 21:03:14 +0100

    Started constructing of new ItemBuilder.java

--- src/main/java/com/arwenmc/api/ItemBuilder/ItemBuilder.java
+++ src/main/java/com/arwenmc/api/ItemBuilder/ItemBuilder.java
@@ -1,4 +1,30 @@
 package com.arwenmc.api.ItemBuilder;
 
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 public class ItemBuilder {
+    private Material material;
+    private int amount;
+    private ItemStack itemStack;
+    private ItemMeta itemMeta;
+
+    public ItemBuilder(Material material, int amount) {
+        this.material = material;
+        this.amount = amount;
+        this.itemStack = new ItemStack(this.material);
+        this.itemMeta = this.itemStack.getItemMeta();
+    }
+
+    public ItemBuilder setDisplayName(String name) {
+        this.itemMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+        return this;
+    }
+
+    public ItemBuilder build() {
+        this.itemStack.setItemMeta(this.itemMeta);
+        return this;
+    }
 }
