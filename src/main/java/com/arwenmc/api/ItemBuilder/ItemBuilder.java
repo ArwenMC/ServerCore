0abf0ff4f7e6830851217c641523a85ffd00280d
Christopher Nethercott <ccnethercott@gmail.com>
2018-07-24 22:00:14 +0100

    Ported over some old code made it better etc etc

--- src/main/java/com/arwenmc/api/ItemBuilder/ItemBuilder.java
+++ src/main/java/com/arwenmc/api/ItemBuilder/ItemBuilder.java
@@ -2,8 +2,21 @@ package com.arwenmc.api.ItemBuilder;
 
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
 
 public class ItemBuilder {
     private Material material;
@@ -40,6 +53,63 @@ public class ItemBuilder {
     public ItemBuilder setDisplayName(String name) {
         this.itemMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
         return this;
+    }
+
+    public ItemBuilder addEnchant(Enchantment enchantment, int level) {
+        this.itemMeta.addEnchant(enchantment, level, true);
+        return this;
+    }
+
+    public ItemBuilder addEnchants(Map<Enchantment, Integer> enchantments) {
+        if (!enchantments.isEmpty()) {
+            for (Enchantment enchantment : enchantments.keySet()) {
+                this.itemMeta.addEnchant(enchantment, enchantments.get(enchantment).intValue(), true);
+            }
+        }
+        return this;
+    }
+
+    public ItemBuilder addItemFlag(ItemFlag itemFlag) {
+        this.itemMeta.addItemFlags(itemFlag);
+        return this;
+    }
+
+    public ItemBuilder setLore(List<String> lores) {
+        ArrayList<String> colouredLores = new ArrayList<>();
+        for (String lore : lores) {
+            colouredLores.add(ChatColor.translateAlternateColorCodes('&', lore));
+        }
+        this.itemMeta.setLore(colouredLores);
+        return this;
+    }
+
+    public ItemBuilder setUnbreakable(boolean unbreakable) {
+        this.itemMeta.setUnbreakable(unbreakable);
+        return this;
+    }
+
+    public String toBase64() {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+
+            dataOutput.writeObject(this.itemStack);
+            dataOutput.close();
+            return Base64Coder.encodeLines(outputStream.toByteArray())
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to save ItemStack", e);
+        }
+    }
+
+    public ItemBuilder fromBase64(String from) {
+        try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(from));
+            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+            this.itemStack = ((ItemStack) dataInput.readObject());
+            dataInput.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
