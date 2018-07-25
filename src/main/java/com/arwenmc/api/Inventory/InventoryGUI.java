6aca88d416bc08e56513d14f1771465097cf6d8c
Christopher Nethercott <ccnethercott@gmail.com>
2018-07-25 22:17:39 +0100

    Added the class row - this is code copied from my own

--- src/main/java/com/arwenmc/api/Inventory/InventoryGUI.java
+++ src/main/java/com/arwenmc/api/Inventory/InventoryGUI.java
@@ -1,9 +1,12 @@
 package com.arwenmc.api.Inventory;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.UUID;
 
 public class InventoryGUI {
@@ -13,12 +16,44 @@ public class InventoryGUI {
     private onClick click;
     private ArrayList<UUID> viewing = new ArrayList<>();
 
-    public InventoryGUI(int rows, onClick click) {
-        this.id = new UUID();
-        this.size = rows * 9 // there are 9 slots on a row
+    public InventoryGUI(String name, int rows, onClick click) {
+        this.id = UUID.randomUUID();
+        this.size = rows * 9; // there are 9 slots on a row
     }
 
     public interface onClick {
         boolean onClick(Player click, InventoryGUI menu, Row row, int slow, ItemStack itemStack);
+    }
+
+    public class Row {
+        private ItemStack[] rowItems = new ItemStack[9];
+        private int row;
+
+        public Row(int row, ItemStack[] itemStacks) {
+            this.row = row;
+            for (int j = 0, i = (row * 9); i < (row * 9) + 9; i++, j++) {
+                this.rowItems[j] = itemStacks[i];
+            }
+        }
+
+        public ItemStack[] getRowItems() {
+            return this.rowItems;
+        }
+
+        public ItemStack getRowItem(int item) {
+            return rowItems[item] == null ? new ItemStack(Material.AIR) : rowItems[item];
+        }
+
+        public int getRow() {
+            return this.row;
+        }
+    }
+
+    private ItemStack getItem(ItemStack itemStack, String name, String... lore) {
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(name);
+        itemMeta.setLore(Arrays.asList(lore));
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
     }
 }
