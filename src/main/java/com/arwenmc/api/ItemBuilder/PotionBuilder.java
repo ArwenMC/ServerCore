73276c70bd6485a11d04c53fce7a3b7b9e3197d2
Christopher Nethercott <ccnethercott@gmail.com>
2018-07-25 08:38:00 +0100

    Updated PotionBuilder

--- src/main/java/com/arwenmc/api/ItemBuilder/PotionBuilder.java
+++ src/main/java/com/arwenmc/api/ItemBuilder/PotionBuilder.java
@@ -16,45 +16,26 @@ import org.bukkit.potion.PotionEffectType;
             .build();
  */
 
-
-public class PotionBuilder
-        extends ItemBuilder {
-    private PotionMeta pm;
-
-    public PotionBuilder(ItemStack itemStack) {
-        super(itemStack);
-    }
+public class PotionBuilder extends ItemBuilder {
+    private PotionMeta potionMeta;
+    private ItemStack itemStack;
 
     public PotionBuilder(Material material, int amount) {
-        super(material, amount);
-    }
-
-    public PotionBuilder(int id, int amount) {
-        super(id, amount);
-    }
-
-    @Deprecated
-    public PotionBuilder(Material material, int amount, int subid) {
-        super(material, amount, subid);
-    }
-
-    @Deprecated
-    public PotionBuilder(int id, int amount, int subid) {
-        super(id, amount, subid);
+        super(Material.POTION, amount);
+        this.itemStack = this.getItemStack();
+        this.potionMeta = (PotionMeta) this.getItemMeta();
     }
 
     public PotionBuilder setColor(Color color) {
-        this.pm = ((PotionMeta) this.is.getItemMeta());
-        this.pm.setColor(color);
-        this.is.setItemMeta(this.pm);
+        this.potionMeta.setColor(color);
+        this.itemStack.setItemMeta(this.potionMeta);
         return this;
     }
 
     public PotionBuilder addCustomEffect(PotionEffectType type, int duration, int amplifier) {
-        this.pm = ((PotionMeta) this.is.getItemMeta());
         PotionEffect effect = new PotionEffect(type, duration, amplifier, true);
-        this.pm.addCustomEffect(effect, true);
-        this.is.setItemMeta(this.pm);
+        this.potionMeta.addCustomEffect(effect, true);
+        this.itemStack.setItemMeta(this.potionMeta);
         return this;
     }
 }
