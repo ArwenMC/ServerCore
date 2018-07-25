1374117a1cddf09195d3a4c0955be9a1044fd72d
Christopher Nethercott <ccnethercott@gmail.com>
2018-07-25 21:52:28 +0100

    Added constructor + an interface

--- src/main/java/com/arwenmc/api/Inventory/InventoryGUI.java
+++ src/main/java/com/arwenmc/api/Inventory/InventoryGUI.java
@@ -1,4 +1,24 @@
 package com.arwenmc.api.Inventory;
 
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class InventoryGUI {
+
+    private UUID id;
+    private int size;
+    private onClick click;
+    private ArrayList<UUID> viewing = new ArrayList<>();
+
+    public InventoryGUI(int rows, onClick click) {
+        this.id = new UUID();
+        this.size = rows * 9 // there are 9 slots on a row
+    }
+
+    public interface onClick {
+        boolean onClick(Player click, InventoryGUI menu, Row row, int slow, ItemStack itemStack);
+    }
 }
