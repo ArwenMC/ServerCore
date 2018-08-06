ba656b4d38040cc884b28051a2a9f9148d85a1cd
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-06 12:55:29 +0200

    Added basic stuff"

--- src/main/java/com/arwenmc/SCPermission.java
+++ src/main/java/com/arwenmc/SCPermission.java
@@ -1,5 +1,6 @@
 package com.arwenmc;
 
+import org.bukkit.Bukkit;
 import org.bukkit.permissions.Permission;
 
 public class SCPermission {
@@ -7,13 +8,23 @@ public class SCPermission {
     public Permission SC_ADMIN = new Permission("sc.admin");
     public Permission SC_PLAYER = new Permission("sc.player");
 
+    Permission[] allPermissions = {
+            SC_ADMIN, SC_PLAYER
+    };
+
 
     ServerCore plugin;
     public SCPermission(ServerCore instance) {
         plugin = instance;
+
+        updatePermissions(); // when the server is loaded this will be ran but I will put in a command so that you can update them.
     }
 
-    
+    public void updatePermissions() {
+        for (Permission permission : allPermissions) {
+            Bukkit.getPluginManager().addPermission(permission);
+        }
+    }
 
 
 
