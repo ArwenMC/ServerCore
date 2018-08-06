c8b53cc8e35339c9ffeb73d51644ffd4df1c9300
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-06 19:13:31 +0200

    Allowed getting the permission from config path

--- src/main/java/com/arwenmc/SCPermission.java
+++ src/main/java/com/arwenmc/SCPermission.java
@@ -1,5 +1,6 @@
 package com.arwenmc;
 
+import org.bukkit.Bukkit;
 import org.bukkit.permissions.Permission;
 
 public enum SCPermission {
@@ -20,4 +21,24 @@ public enum SCPermission {
         return new Permission(this.permissionNode);
     }
 
+    public Permission getCommandPermission(String configPath) {
+        switch (new ServerCore().getConfig().getString(configPath)) {
+            case "admin":
+                return ADMIN.getPermission();
+            case "staff":
+                return STAFF.getPermission();
+            case "donator":
+                return DONATOR.getPermission();
+            case "member":
+                return MEMBER.getPermission();
+            case "player":
+                return PLAYER.getPermission();
+            default:
+                try {
+                    throw new IllegalAccessException();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
 }
