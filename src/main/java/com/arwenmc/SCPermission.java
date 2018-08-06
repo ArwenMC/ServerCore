bd88b0da80ecdde36618aa03b706c05c548f57e6
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-06 18:42:54 +0200

    Enum has been started

--- src/main/java/com/arwenmc/SCPermission.java
+++ src/main/java/com/arwenmc/SCPermission.java
@@ -1,45 +1,19 @@
 package com.arwenmc;
 
-import org.bukkit.Bukkit;
-import org.bukkit.permissions.Permission;
+public enum SCPermission {
 
-public class SCPermission {
+    ADMIN("sc_admin"),
+    STAFF("sc.staff"),
+    MEMBER("sc.member"),
+    PLAYER("sc.player");
 
-    public Permission SC_ADMIN = new Permission("sc.admin");
-    public Permission SC_STAFF = new Permission("sc.staff");
-    public Permission SC_MEMBER = new Permission("sc.member");
-    public Permission SC_PLAYER = new Permission("sc.player");
+    private String permissionNode;
 
-    Permission[] allPermissions = {
-            SC_ADMIN, SC_STAFF, SC_MEMBER, SC_PLAYER
-    };
-
-
-    ServerCore plugin;
-    public SCPermission(ServerCore instance) {
-        plugin = instance;
-
-        updatePermissions(); // when the server is loaded this will be ran but I will put in a command so that you can update them.
+    SCPermission(String permissionNode) {
+        this.permissionNode = permissionNode;
     }
 
-    public void updatePermissions() {
-        for (Permission permission : allPermissions) {
-            Bukkit.getPluginManager().addPermission(permission);
-        }
-    }
-
-    public Permission getPermission(String config) throws IllegalAccessException {
-        switch (plugin.getConfig().getString(config)) {
-            case "player":
-                return this.SC_PLAYER;
-            case "member":
-                return this.SC_MEMBER;
-            case "staff":
-                return this.SC_STAFF;
-            case "admin":
-                return this.SC_ADMIN;
-            default:
-                throw new IllegalAccessException();
-        }
+    public String getPermission() {
+        return this.permissionNode;
     }
 }
