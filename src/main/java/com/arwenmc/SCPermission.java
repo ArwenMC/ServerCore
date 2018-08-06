f223bba2eca8c7f076d1c8d1de28098eece5cd30
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-06 18:33:19 +0200

    Player should replace none

--- src/main/java/com/arwenmc/SCPermission.java
+++ src/main/java/com/arwenmc/SCPermission.java
@@ -7,10 +7,11 @@ public class SCPermission {
 
     public Permission SC_ADMIN = new Permission("sc.admin");
     public Permission SC_STAFF = new Permission("sc.staff");
+    public Permission SC_MEMBER = new Permission("sc.member");
     public Permission SC_PLAYER = new Permission("sc.player");
 
     Permission[] allPermissions = {
-            SC_ADMIN, SC_STAFF, SC_PLAYER
+            SC_ADMIN, SC_STAFF, SC_MEMBER, SC_PLAYER
     };
 
 
@@ -24,6 +25,13 @@ public class SCPermission {
     public void updatePermissions() {
         for (Permission permission : allPermissions) {
             Bukkit.getPluginManager().addPermission(permission);
+        }
+    }
+
+    public Permission getPermission(String config) {
+        switch (plugin.getConfig().getString(config)) {
+            case "none":
+
         }
     }
 }
