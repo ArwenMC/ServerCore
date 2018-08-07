9c10a21cf96ecc7e6813af17ab7bf6ffe721dc31
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-07 20:36:29 +0200

    Rewritten Scoreboard Builder

--- src/main/java/com/arwenmc/api/ScoreboardBuilder/ScoreboardBuilder.java
+++ src/main/java/com/arwenmc/api/ScoreboardBuilder/ScoreboardBuilder.java
@@ -1,43 +1,41 @@
 package com.arwenmc.api.ScoreboardBuilder;
 
-/*
- Example Of ScoreboardBuilder
-
- Scoreboard sb = new ScoreboardBuilder("yourObjective", "yourTitle", Arrays.asList("yourScore", "yourNextScore"))
-            .build();
- player.setScoreboard(sb);
- */
-
-/*public class ScoreboardBuilder {
-    private ScoreboardManager mg = Bukkit.getScoreboardManager();
-    private Scoreboard sb = this.mg.getMainScoreboard();
-
-    public ScoreboardBuilder(String objective, String title, List<String> entrys) {
-        Objective obj = this.sb.registerNewObjective(objective, "dummy");
-        int i = 0;
-        for (String score : Lists.reverse(entrys)) {
-            i++;
-            obj.getScore(score).setScore(i);
-        }
-        obj.setDisplayName(title);
-        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
-    }
-
-    public Scoreboard build() {
-        return this.sb;
-    }
-}*/
-
+import com.google.common.collect.Lists;
 import org.bukkit.Bukkit;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
+
+import java.util.List;
 
 public class ScoreboardBuilder {
 
     private ScoreboardManager scoreboardManager = Bukkit.getScoreboardManager();
     private Scoreboard scoreboard = this.scoreboardManager.getMainScoreboard();
+    private Objective objective;
 
     public ScoreboardBuilder(String objectiveName) {
+        this.objective = this.scoreboard.registerNewObjective(objectiveName, "dummy", objectiveName);
+    }
+
+    public ScoreboardBuilder addEntries(List<String> entries) {
+        int i = 0;
+        for (String score : Lists.reverse(entries)) {
+            i++;
+            objective.getScore(score).setScore(i);
+        }
+        return this;
+    }
+
+    public ScoreboardBuilder setTitle(String title) {
+        objective.setDisplayName(title);
+        return this;
+    }
+
+    public Scoreboard build() {
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+        return this.scoreboard;
     }
 
 }
