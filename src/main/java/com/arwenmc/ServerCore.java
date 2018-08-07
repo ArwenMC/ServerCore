af065f506474ee764a0187bdd7b0a4419d551ce3
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-07 11:05:02 +0200

    reformat

--- src/main/java/com/arwenmc/ServerCore.java
+++ src/main/java/com/arwenmc/ServerCore.java
@@ -82,12 +82,12 @@ public class ServerCore extends JavaPlugin {
                     ItemStack grayDye = new ItemStack(Material.GRAY_DYE);
                     ItemStack limeDye = new ItemStack(Material.LIME_DYE);
 
-                    toggleGUI = new InventoryGUI("&aTest GUI", 4, PLUGIN, (clicker, menu, row, slot, item)  -> {
-                        if(item.getType().equals(Material.INK_SAC)) {
-                            if(item.equals(grayDye)) {
+                    toggleGUI = new InventoryGUI("&aTest GUI", 4, PLUGIN, (clicker, menu, row, slot, item) -> {
+                        if (item.getType().equals(Material.INK_SAC)) {
+                            if (item.equals(grayDye)) {
                                 toggleGUI.setSlot(toggleGUI.getRow(2), 4, limeDye, "&aEnabled", "&7Click to disable");
                                 player.sendMessage(ChatColor.GREEN + "You enabled this");
-                            } else if(item.equals(limeDye)) {
+                            } else if (item.equals(limeDye)) {
                                 toggleGUI.setSlot(toggleGUI.getRow(2), 4, grayDye, "&cDisabled", "&7Click to enable");
                                 player.sendMessage(ChatColor.RED + "You disabled this");
                             }
@@ -95,7 +95,7 @@ public class ServerCore extends JavaPlugin {
 
                         toggleGUI.refresh(player.getUniqueId());
                         return true;
-                        });
+                    });
                     toggleGUI.setSlot(toggleGUI.getRow(1), 4, new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE), "&aEnable or Disable");
                     toggleGUI.setSlot(toggleGUI.getRow(2), 4, grayDye, "&cDisabled", "&7Click to enable");
                     toggleGUI.open(player.getUniqueId());
