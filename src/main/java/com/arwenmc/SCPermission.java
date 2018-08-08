2e7db6e2422c9c5f53a13426e43afd4a1c5155dd
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-08 11:43:27 +0200

    Updated SCPermission and wrote an example command with fly. I haven't put in the flying logic yet

--- src/main/java/com/arwenmc/SCPermission.java
+++ src/main/java/com/arwenmc/SCPermission.java
@@ -3,41 +3,58 @@ package com.arwenmc;
 import org.bukkit.Bukkit;
 import org.bukkit.permissions.Permission;
 
-public enum SCPermission {
-
-    ADMIN("sc.admin"),
-    STAFF("sc.staff"),
-    DONATOR("sc.donator"),
-    MEMBER("sc.member"),
-    PLAYER("sc.player");
-
-    private String permissionNode;
-    private Permission permission;
-
-    SCPermission(String permissionNode) {
-        this.permissionNode = permissionNode;
-        this.permission = new Permission(this.permissionNode);
-    }
-
-    public Permission getPermission() {
-        return new Permission(this.permissionNode);
-    }
+public class SCPermission {
 
     public Permission getCommandPermission(String configPath) {
         switch (new ServerCore().getConfig().getString(configPath)) {
             case "admin":
-                return ADMIN.getPermission();
+                return groupPermissions.ADMIN.getPermission();
             case "staff":
-                return STAFF.getPermission();
+                return groupPermissions.STAFF.getPermission();
             case "donator":
-                return DONATOR.getPermission();
+                return groupPermissions.DONATOR.getPermission();
             case "member":
-                return MEMBER.getPermission();
+                return groupPermissions.MEMBER.getPermission();
             case "player":
-                return PLAYER.getPermission();
+                return groupPermissions.PLAYER.getPermission();
             default:
                 Bukkit.getLogger().severe("That group doesn't exist, and so the command may work without any permission checks. I'd highly suggest checking your config.");
                 throw new IllegalArgumentException("That group doesn't exist, please check your config.");
+        }
+    }
+
+    public enum configPath {
+        FLY("fly.fly_permission"),
+        GAMEMODE("gamemode.gamemode_permission"),
+        CHAT("chat.chat_permission"),
+        HELP("help.help_permission");
+
+        private String configPath;
+
+        configPath(String configPath) {
+            this.configPath = configPath;
+        }
+
+        public String getConfigPath() {
+            return this.configPath;
+        }
+    }
+
+    public enum groupPermissions {
+        ADMIN("sc.admin"),
+        STAFF("sc.staff"),
+        DONATOR("sc.donator"),
+        MEMBER("sc.member"),
+        PLAYER("sc.player");
+
+        private String permissionNode;
+
+        groupPermissions(String permissionNode) {
+            this.permissionNode = permissionNode;
+        }
+
+        public Permission getPermission() {
+            return new Permission(this.permissionNode);
         }
     }
 }
---
+++ src/main/java/com/arwenmc/commands/FlyCommand.java
@@ -0,0 +1,33 @@
+package com.arwenmc.commands;
+
+import com.arwenmc.SCPermission;
+import com.arwenmc.ServerCore;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class FlyCommand implements CommandExecutor {
+
+    ServerCore plugin;
+    public FlyCommand(ServerCore instance) {
+        plugin = instance;
+    }
+
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+        if (!(commandSender instanceof Player)) {
+            commandSender.sendMessage(plugin.NOT_PLAYER);
+            return true;
+        } else {
+            Player player = (Player) commandSender;
+            if (player.hasPermission(new SCPermission().getCommandPermission(SCPermission.configPath.FLY.getConfigPath()))) {
+                // flying logic here.
+                return true;
+            } else {
+                player.sendMessage(plugin.NO_PERMISSION);
+                return true;
+            }
+        }
+    }
+
+}
