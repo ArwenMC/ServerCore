8100b1a0db0ac9a09cf7be525fd06349aa29cc4b
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-09 10:11:08 +0200

    added required method

--- src/main/java/com/arwenmc/commands/ChatCommand.java
+++ src/main/java/com/arwenmc/commands/ChatCommand.java
@@ -1,4 +1,19 @@
 package com.arwenmc.commands;
 
-public class ChatCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+public class ChatCommand implements CommandExecutor, Listener {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+        if (!(commandSender instanceof Player)) {
+
+        }
+    }
+
+
 }
