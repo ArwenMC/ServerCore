40c32628fb81ea86cdbe710e7a4105f38dcf4283
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-09 10:18:14 +0200

    Added basic stuff class structure

--- src/main/java/com/arwenmc/commands/ChatCommand.java
+++ src/main/java/com/arwenmc/commands/ChatCommand.java
@@ -1,18 +1,42 @@
 package com.arwenmc.commands;
 
+import com.arwenmc.SCPermission;
+import com.arwenmc.ServerCore;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatCommand implements CommandExecutor, Listener {
+
+    ServerCore plugin;
+    public ChatCommand(ServerCore instance) {
+        plugin = instance;
+    }
+
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
         if (!(commandSender instanceof Player)) {
 
+        } else {
+            Player player = (Player) commandSender;
+            if (new SCPermission().permissionCheck(player, SCPermission.ConfigPath.CHAT)) {
+
+            } else {
+                player.sendMessage(plugin.NO_PERMISSION);
+                return true;
+            }
         }
+    }
+
+    @EventHandler
+    public void onPlayerChatEvent(AsyncPlayerChatEvent event) {
+
+
     }
 
 
