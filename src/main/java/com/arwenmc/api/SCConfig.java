8721a85256ed2027456b9e2df68c72c7bef5df7c
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-10 19:22:47 +0200

    Added Config to SCConfig & fixed typo in cxonfig

--- src/main/java/com/arwenmc/api/SCConfig.java
+++ src/main/java/com/arwenmc/api/SCConfig.java
@@ -1,4 +1,29 @@
 package com.arwenmc.api;
 
 public class SCConfig {
+
+    public enum Database {
+        DB_ENABLED("database.db_enabled"),
+        DB_TYPE("database.database_type"),
+        DB_HOST("database.host"),
+        DB_PORT("database.port"), // should be int
+        DB_DATABASE("database.database"),
+        DB_USERNAME("database.username"),
+        DB_PASSWORD("database.password");
+
+        private String configPath;
+
+        Database(String configPath) {
+            this.configPath = configPath
+        }
+
+        public String getConfigPath() {
+            return this.configPath;
+        }
+
+    }
+
+
+
+
 }
