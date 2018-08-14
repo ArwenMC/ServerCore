083fbb7055d9ac00fe588648e706c8b03c9fbebe
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-14 14:36:55 +0200

    Added some basic things

--- src/main/java/com/arwenmc/SCConfig.java
+++ src/main/java/com/arwenmc/SCConfig.java
@@ -2,6 +2,8 @@ package com.arwenmc;
 
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
 
 public class SCConfig {
 
@@ -15,5 +17,28 @@ public class SCConfig {
     public String getAndColour(String configPath) {
         String configValue = config.getString(configPath);
         return ChatColor.translateAlternateColorCodes('&', configValue);
+    }
+
+    public boolean permissionCheck(Player player, PermissionNode permissionNode) {
+        if (player.hasPermission(permissionNode.getPermission())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public enum PermissionNode {
+        TEST("test.test"),
+        TEST2("test.test2");
+
+        private Permission permission;
+
+        public PermissionNode(String permissionPath) {
+            this.permission = new Permission(permissionPath);
+        }
+
+        public Permission getPermission() {
+            return this.permission;
+        }
     }
 }
