7dccc4a48da72882dfb0a7f9a7ccaef7d0cce932
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-14 13:26:05 +0200

    Rewritting this completely

--- src/main/java/com/arwenmc/commands/FlyCommand.java
+++ src/main/java/com/arwenmc/commands/FlyCommand.java
@@ -1,7 +1,6 @@
 package com.arwenmc.commands;
 
 import com.arwenmc.ServerCore;
-import com.arwenmc.SCConfig;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
