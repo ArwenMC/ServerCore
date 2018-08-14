6569c2ee1a0aeb6d9571240add9a23e09f76e8fb
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-14 14:29:41 +0200

    Added the basic class + getAndColor submethod

--- src/main/java/com/arwenmc/SCConfig.java
+++ src/main/java/com/arwenmc/SCConfig.java
@@ -1,4 +1,21 @@
 package com.arwenmc;
 
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+
 public class SCConfig {
+
+    ServerCore plugin;
+    FileConfiguration config;
+    public SCConfig(ServerCore plugin) {
+        this.plugin = plugin;
+        this.config = plugin.getConfig()
+    }
+
+    public String getAndColour(String configPath) {
+        String configValue = config.getString(configPath);
+        return ChatColor.translateAlternateColorCodes('&', configPath);
+    }
+
 }
