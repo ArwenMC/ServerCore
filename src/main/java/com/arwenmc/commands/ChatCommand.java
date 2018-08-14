4eeee3b00ba94d4b1fff60c0993a12754fb60a5f
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-14 17:55:01 +0200

    Added some stuff to ChatCommand

--- src/main/java/com/arwenmc/commands/ChatCommand.java
+++ src/main/java/com/arwenmc/commands/ChatCommand.java
@@ -1,11 +1,16 @@
 package com.arwenmc.commands;
 
+import com.arwenmc.SCConfig;
 import com.arwenmc.ServerCore;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-public class ChatCommand implements CommandExecutor {
+public class ChatCommand implements CommandExecutor, Listener {
 
     ServerCore plugin;
     public ChatCommand(ServerCore plugin) {
@@ -14,8 +19,24 @@ public class ChatCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+        if (!(commandSender instanceof Player)) {
+            commandSender.sendMessage(plugin.NOT_PLAYER);
+            return true;
+        } else {
+            Player player = (Player) commandSender;
+            if (plugin.config.permissionCheck(player, SCConfig.CommandPermissions.CHAT)) {
 
+            } else {
+                player.sendMessage(plugin.NO_PERMISSION);
+                return true;
+            }
+        }
         return false;
+    }
+
+    @EventHandler
+    public void onPlayerChatEvent(AsyncPlayerChatEvent event) {
+        //
     }
 
 }
